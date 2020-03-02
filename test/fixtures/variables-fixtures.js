0393964e6bc1aa1f908426a42b7187b59a1846a9
patriciaSR <patricia.suarez.psr@gmail.com>
2020-03-02 11:18:26 +0100

    remove photos

--- test/fixtures/variables-fixtures.js
+++ test/fixtures/variables-fixtures.js
@@ -24,23 +24,5 @@ const mockFilms = [
   },
 ];
 
-const totoroImg = 'https://i.pinimg.com/originals/f7/f8/4d/f7f84dc6d93cb70b5ea612fd26f5dd24.jpg';
+export { ENDPOINTGOOD, ENDPOINTBAD, mockFilms };
 
-const mockPhotos = [
-  {
-    name: 'bambi',
-    photo: 'bambiURL',
-  },
-  {
-    name: 'dumbo',
-    photo: 'dumboURL',
-  },
-];
-
-export {
-  ENDPOINTGOOD,
-  ENDPOINTBAD,
-  mockFilms,
-  mockPhotos,
-  totoroImg,
-};
